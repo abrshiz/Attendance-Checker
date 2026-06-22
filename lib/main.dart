bcbe34532b9b349626588fa324a94af36a236e8d
Abrham Wendesen <abrshiz@yahoo.com>
2026-06-22 23:18:09 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
